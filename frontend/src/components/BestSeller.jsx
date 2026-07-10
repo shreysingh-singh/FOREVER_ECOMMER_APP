9744df4630e1720fe7a7c651137fb4932894f980
Shrey <shreysingh11733@gmail.com>
2026-07-10 14:10:57 +0530

    _05_BestSeller_Section

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,48 @@
+
+import { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopingContext'
+import Tittle from './Tittle';
+import ProductItem from './ProductItem';
+
+function BestSeller() {
+    const { products } = useContext(ShopContext);
+    const [bestSeller, setBestSeller] = useState([]);
+
+    useEffect(() => {
+      console.log("Products:", products.length);
+
+      const bestProduct = products.filter((item) => item.bestseller);
+
+      console.log("Filtered:", bestProduct.length);
+      console.log(bestProduct);
+
+      setBestSeller(bestProduct.slice(0, 5));
+    }, [products]);
+    
+    console.log(products[0]);
+
+  return (
+    <div className="my-10">
+      <div className="text-center text-3xl py-8">
+        <Tittle text1={"BEST"} text2={"SELLERS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem Ipsum is supply dummy text of the printing and typesetting
+          industry. Lorem Ispum hase been the.
+        </p>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {bestSeller.map((item) => (
+          <ProductItem
+            key={item._id}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BestSeller
