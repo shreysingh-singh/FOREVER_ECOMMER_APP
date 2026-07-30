8a347de59371a9c83025990adce8859bd39cc737
Shrey <shreysingh11733@gmail.com>
2026-07-30 13:13:58 +0530

    _15_Contact_US_Page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,70 @@
 import React from 'react'
+import Tittle from '../components/Tittle'
+import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox';
 
 function About() {
   return (
     <div>
-      
+      <div className="text-2xl text-center pt-8 border-t">
+        <Tittle text1={"ABOUT"} text2={"US"} />
+      </div>
+
+      <div className="flex flex-col my-20 md:flex-row gap-16">
+        <img className="w-full md:max-w-[450px]" src={assets.about_img} />
+        <div className="flex flex-col justify-center gap-6  md:w-2/4 text-gray-600">
+          <p>
+            Forever was born out of a passion for innovation and a desire to
+            revolutionize the way people shop online. Our journey began with a
+            simple idea: to provide a platform where customers can easily
+            discover, explore, and purchase a wide range of products from the
+            comfort of their homes
+          </p>
+          <p>
+            Since our inception, we've worked tirelessly to curate a diverse
+            selection of high-quality products that cater to every taste and
+            preference. From fashion and beauty to electronics and home
+            essentials, we offer an extensive collection sourced from trusted
+            brands and suppliers.
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Our mission at Forever is to empower customers with choice,
+            convenience, and confidence. We're dedicated to providing a seamless
+            shopping experience that exceeds expectations, from browsing and
+            ordering to delivery and beyond.
+          </p>
+        </div>
+      </div>
+      <div className="text-xl py-4">
+        <Tittle text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-500">
+            We Meticulously Select And Vet Each Product To Ensuer It Meets Our
+            Stringent Quality Standars
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-500">
+            We Meticulously Select And Vet Each Product To Ensuer It Meets Our
+            Stringent Quality Standars
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-500">
+            We Meticulously Select And Vet Each Product To Ensuer It Meets Our
+            Stringent Quality Standars
+          </p>
+        </div>
+      </div>
+      <NewsLetterBox />
     </div>
-  )
+  );
 }
 
 export default About
